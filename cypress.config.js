ade7c6f8a2b0c36af85367f028af5373c3ec4681
Thanchila Pirasanthan <than.pirasanthan@vivup.co.uk>
2024-06-11 21:47:30 +0100

    Add .gitattributes file to handle line endings

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
+    "chromeWebSecurity": false,
     pageLoadTimeout: 120000,
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
+      
       // implement node event listeners here
     },
   },
--- cypress/e2e/login_feature.cy.js
+++ cypress/e2e/login_feature.cy.js
@@ -5,7 +5,13 @@ describe('launch_website and login', function () {
 cy.visit("https://www.way2automation.com/way2auto_jquery/");
   })
   
-  it('test1', () => {
+  it('test1',() => {
+    cy.title('Welcome to the Test Site');
+
+  })
+
+
+  it('test2', () => {
       cy.get('input[name="name"]').type('Firstname');
       cy.get('input[name="phone"]').type('Lastname');
       cy.get('input[name="email"]').type('testing@example.com');
@@ -16,15 +22,17 @@ cy.visit("https://www.way2automation.com/way2auto_jquery/");
       cy.get(':nth-child(11) > input').type('Test1234!');
       cy.get(':nth-child(12) > .span_1_of_4 > .button').click();
       cy.get('#alert').contains('This is just a dummy form, you just clicked SUBMIT BUTTON');
+
   });
 
-  it('test2',() => {
-    cy.title('Welcome to the Test Site');
+ 
+  it('test3', () => {
+    cy.get('a.fancybox[href="#login"]').click();
+    cy.get('#login > #load_form > :nth-child(5) > input').type('Username123');
+    cy.get('#login > #load_form > :nth-child(6) > input').type('Testing_Password1234!');
+    cy.get(':nth-child(8) > .span_3_of_4 > :nth-child(1) > .fancybox').click();
+    
 
-  })
-
-
-});
+})});
 
       
-  
