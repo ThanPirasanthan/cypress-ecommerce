4d0cc5e2f17d8af23264722ddd735bdbf29b5412
Thanchila Pirasanthan <than.pirasanthan@vivup.co.uk>
2024-06-12 21:44:32 +0100

    added ways of not recording dynamic ID of elements

--- cypress/e2e/accordion_feature.cy.js
+++ cypress/e2e/accordion_feature.cy.js
@@ -4,21 +4,21 @@ describe('allow elements to be moved using the mouse', function () {
       cy.visit("https://www.way2automation.com/way2auto_jquery/accordion.php#load_box");
     });
   
-    it.only('test1', () => {
-        cy.get('#accordion > h3').eq(0).click();
-        cy.get('#accordion > div').eq(0).should('be.visible').and('contain', 'Content for section 1.');
+    it('test1', () => {
+        cy.get('.active > a').click();
+        cy.get('.active > a').click();
     
-        // Collapse Section 1
-        cy.get('#accordion > h3').eq(0).click();
-        cy.get('#accordion > div').eq(0).should('not.be.visible');
+        // // Collapse Section 1
+        // cy.get('#accordion > h3').eq(0).click();
+        // cy.get('#accordion > div').eq(0).should('not.be.visible');
     
-        // Test Section 2
-        cy.get('#accordion > h3').eq(1).click();
-        cy.get('#accordion > div').eq(1).should('be.visible').and('contain', 'Content for section 2.');
+        // // Test Section 2
+        // cy.get('#accordion > h3').eq(1).click();
+        // cy.get('#accordion > div').eq(1).should('be.visible').and('contain', 'Content for section 2.');
     
-        // Collapse Section 2
-        cy.get('#accordion > h3').eq(1).click();
-        cy.get('#accordion > div').eq(1).should('not.be.visible');
+        // // Collapse Section 2
+        // cy.get('#accordion > h3').eq(1).click();
+        // cy.get('#accordion > div').eq(1).should('not.be.visible');
     });
   });
   
